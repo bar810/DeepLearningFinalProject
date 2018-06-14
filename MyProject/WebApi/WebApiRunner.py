d705b77507ece3099480673d293f13b4260e5dff
barbrownshtein <bar810@gmail.com>
2018-06-14 23:20:14 +0300

    classifier added

--- MyProject/WebApi/WebApiRunner.py
+++ MyProject/WebApi/WebApiRunner.py
@@ -1,17 +1,17 @@
 # THIS WEB-API BASED ON THIS TUTORIAL: https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-import MyProject.NeuralNetworks.RunPrediction as prediction
-
+import MyProject.NeuralNetworks.Food_101.food_cnn_predict_by_query as classifier
 app = Flask(__name__)
 api = Api(app)
+
 
 class Query(Resource):
     def get(self,arg):
         print("Input: " + arg)
-        result=prediction.runPrediction(arg)
-        print("Result: "+result)
-        return result,200
+        res=classifier.predict(arg)
+        print("result: "+ res)
+        return res,200
 
 # QUERY SHOULD SEND TO 127.0.0.1:5000/PATH_TO_INPUT
 api.add_resource(Query, "/<string:arg>")
