30cdb5e71133c3fc5dc0277045eacc7d6201ba9e
bar810 <bar810@gmail.com>
2018-07-06 15:01:57 +0300

    peapre the first network and add graph

--- MyProject/NeuralNetworks/Food_101/food_train.py
+++ MyProject/NeuralNetworks/Food_101/food_train.py
@@ -4,49 +4,64 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import os
 
 # Initialize the CNN
-classifier = Sequential()
-CLASSES = 4
-EPOCHS = 25
+model = Sequential()
+BATCH_SIZE=32
+CLASSES = 2
+EPOCHS = 5
+MODEL_SAVE_PATH='./models/model_food_101.h5'
+MODEL_SAVE_PATH_WEIGHTS='./models/weights_food_101.h5'
+TEST_SET_PATH='./data_set/training_set'
+TRAIN_SET_PATH='./data_set/test_set'
+VALIDATION_SET_PATH='./data_set/validation_set'
+
 # Convolution and Max pooling
-classifier.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
-classifier.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Conv2D(64, (3, 3), activation='relu'))
-classifier.add(Conv2D(64, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Conv2D(128, (3, 3), activation='relu'))
-classifier.add(Conv2D(64, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-
+model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 # Flatten
-classifier.add(Flatten())
-
+model.add(Flatten())
 # Full connection
-classifier.add(Dense(128, activation='relu'))
-classifier.add(Dense(CLASSES, activation='softmax'))
-
+model.add(Dense(128, activation='relu'))
+model.add(Dense(CLASSES, activation='softmax'))
 # Compile classifier
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Fitting CNN to the images
 train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-training_set = train_datagen.flow_from_directory('./dataset/training_set', target_size=(128, 128), batch_size=32,
-                                                 class_mode='categorical')
-test_set = test_datagen.flow_from_directory('./dataset/test_set', target_size=(128, 128), batch_size=32,
-                                            class_mode='categorical')
-classifier.fit_generator(training_set, steps_per_epoch=800 / 32, epochs=EPOCHS, validation_data=test_set,
-                         validation_steps=200 / 32)
+training_set = train_datagen.flow_from_directory(TEST_SET_PATH, target_size=(128, 128), batch_size=BATCH_SIZE,class_mode='categorical')
+test_set = test_datagen.flow_from_directory(TRAIN_SET_PATH, target_size=(128, 128), batch_size=BATCH_SIZE,class_mode='categorical')
+validation_set = test_datagen.flow_from_directory(VALIDATION_SET_PATH, target_size=(128, 128), batch_size=BATCH_SIZE,class_mode='categorical')
+
+history= model.fit_generator(training_set, steps_per_epoch=800 / 32, epochs=EPOCHS, validation_data=validation_set,validation_steps=200 / 32)
+
+# results
+score=model.evaluate_generator(test_set)
+print("\nTest score:", score[0])
+print('Test accuracy:', score[1])
+print("finish")
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+print("finish2")
 
 # save model
-import os
-
 target_dir = './models/'
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
-classifier.save('./models/model_food_label_frize.h5')
-classifier.save_weights('./models/weights_food_label_frize.h5')
+model.save(MODEL_SAVE_PATH)
+model.save_weights(MODEL_SAVE_PATH_WEIGHTS)
--- MyProject/WebApi/WebApiRunner.py
+++ MyProject/WebApi/WebApiRunner.py
@@ -1,12 +1,9 @@
 # THIS WEB-API BASED ON THIS TUTORIAL: https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
-import MyProject.NeuralNetworks.Food_101.food_label_predict as foodClassifier
-import MyProject.NeuralNetworks.Food_101.food_non_food_predict as foodNonFoodClassifier
+from flask_restful import Api, Resource
+import MyProject.NeuralNetworks.Food_101.src.food_label_predict as foodClassifier
+import MyProject.NeuralNetworks.Food_101.src.food_non_food_predict as foodNonFoodClassifier
 
-from tkinter import Tk,Label,Canvas,NW,Entry,Button
-
-import urllib
 app = Flask(__name__)
 api = Api(app)
 
