bb49446fe816080ccf81e1bd307aa68305557aa3
alex <sasha.gor@gmail.com>
2018-07-14 22:49:38 +0300

    optimizations

--- MyProject/NeuralNetworks/Cifar_10/food_train.py
+++ MyProject/NeuralNetworks/Cifar_10/food_train.py
@@ -11,7 +11,7 @@ import keras
 model = Sequential()
 BATCH_SIZE=32
 CLASSES = 5
-EPOCHS = 20
+EPOCHS = 50
 MODEL_SAVE_PATH='./models/model_cifar_10.h5'
 MODEL_SAVE_PATH_WEIGHTS='./models/weights_cifar_10.h5'
 TEST_SET_PATH='./data_set/training_set'
@@ -26,7 +26,7 @@ VALIDATION_SET_PATH='./data_set/validation_set'
 # # Convolution and Max pooling
 model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3)))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -34,6 +34,13 @@ model.add(Dropout(0.25))
 model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
 model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -47,7 +54,7 @@ model.add(Activation('softmax'))
 
 
 # initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+opt = keras.optimizers.rmsprop(lr=0.0005, decay=1e-6)
 
 # Let's train the model using RMSprop
 model.compile(loss='categorical_crossentropy',
@@ -56,7 +63,7 @@ model.compile(loss='categorical_crossentropy',
 
 
 # Fitting CNN to the images
-train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 training_set = train_datagen.flow_from_directory(TEST_SET_PATH, target_size=(128, 128), batch_size=BATCH_SIZE,class_mode='categorical')
 test_set = test_datagen.flow_from_directory(TRAIN_SET_PATH, target_size=(128, 128), batch_size=BATCH_SIZE,class_mode='categorical')
@@ -64,7 +71,7 @@ validation_set = test_datagen.flow_from_directory(VALIDATION_SET_PATH, target_si
 
 
 model.summary()
-history= model.fit_generator(training_set, steps_per_epoch=800 / 32, epochs=EPOCHS, validation_data=validation_set,validation_steps=200 / 32)
+history= model.fit_generator(training_set, steps_per_epoch=4757 / BATCH_SIZE, epochs=EPOCHS, validation_data=validation_set,validation_steps=1638 / BATCH_SIZE)
 #
 # results
 score=model.evaluate_generator(test_set)
